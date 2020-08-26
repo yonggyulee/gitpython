2fc6acde1e29888fd8268a6d4b4f47bc9bcfea0e
yonggyulee <yg9677@gmail.com>
2020-08-26 17:29:33 +0900

    #변수와 치환문

--- 01.py
+++ 01.py
@@ -1,1 +1,30 @@
-print('ghhe')
+
+# print('hello wordl')
+# a=2
+# if a>1:
+#     print(a)
+#     print('big')
+#
+# b = 30
+# def my_func(i):
+#     if i >10:
+#         print('big')
+#     else:
+#         print('small')
+#
+#     print('my_finc end....')
+
+
+import keyword
+
+friend = 1
+e = 1
+my_name = '이용규'
+_yourName = '둘리'
+
+print(keyword.kwlist)
+
+가격1 = 2000
+
+print(가격1)
+
