28f23dc672dfa8bea20a01633cf61eae66210dbd
yonggyulee <yg9677@gmail.com>
2020-10-05 11:49:31 +0900

    datetime

--- 02/17.enumerate.py
+++ 02/17.enumerate.py
@@ -5,5 +5,8 @@ for color in ['red', 'yellow', 'blue', 'black', 'gray']:
     print(index, color)
     index = index +1
 
+for item in enumerate(['red', 'yellow', 'blue', 'black', 'gray']):
+    print(item[0], item[1])
+
 for index, color in enumerate(['red', 'yellow', 'blue', 'black', 'gray']):
     print(index, color)
